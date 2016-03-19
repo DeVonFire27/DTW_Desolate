b56e40883b086b2ab96ecad83613edbcf1e8da61
DeVonFire27 <DeVonFire3@gmail.com>
2016-03-19 18:18:26 -0500

    Added files via upload
    code the school provided to help us out

---
+++ SGD Wrappers/SGD_Color.cpp
@@ -0,0 +1,20 @@
+/***************************************************************
+|	File:		SGD_Color.cpp
+|	Author:		Michael Mozdzierz
+|	Date:		04/12/2014
+|
+***************************************************************/
+
+#include "globals.h"
+#include "SGD_Color.h"
+
+
+SGD::Color SGD::Color::Red = Color(255, 0, 0);
+SGD::Color SGD::Color::Orange = Color(255, 64, 0);
+SGD::Color SGD::Color::Yellow = Color(255, 255, 0);
+SGD::Color SGD::Color::Green = Color(0, 255, 0);
+SGD::Color SGD::Color::Blue = Color(0, 0, 255);
+SGD::Color SGD::Color::Purple = Color(128, 0, 255);		
+SGD::Color SGD::Color::White = SGD::Color(255, 255, 255);
+SGD::Color SGD::Color::Black = SGD::Color(0, 0, 0);
+SGD::Color SGD::Color::Gray = SGD::Color(128, 128, 128);
